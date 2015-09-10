a39401c3f1032be349892daff3a11d94082abaae
Liam Armour <liamarmour1991@googlemail.com>
2015-09-10 12:06:59 +0100

    merging the grunt copy file

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,14 @@
-/**
- * Created by liam.armour on 10/09/15.
- */
+(function () {
+    'use strict';
+    var copyTask = require('./.grunt/copy-task');
+
+    module.exports = function(grunt){
+        grunt.initConfig({
+                pkg: grunt.file.readJSON('package.json'),
+                copy: copyTask
+            });
+        )
+        grunt.loadNpmTasks('grunt-contrib-copy');
+        grunt.registerTask('default', ['copy']);
+    };
+})();
