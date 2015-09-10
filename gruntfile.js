47e4c9b9ab2a1af5bb37de3ca5dbef7f7a8e50b7
Liam Armour <liamarmour1991@googlemail.com>
2015-09-10 12:16:43 +0100

    updated the grunt file, this now uses the lint file for less

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,20 @@
 (function () {
     'use strict';
-    var copyTask = require('./.grunt/copy-task');
+    var copyTask = require('./.grunt/copy-task'),
+        lessTask = require('./.grunt/less-task')
 
     module.exports = function(grunt){
         grunt.initConfig({
-                pkg: grunt.file.readJSON('package.json'),
-                copy: copyTask
+            pkg: grunt.file.readJSON('package.json'),
+            copy: copyTask,
+            lesslint: lessTask,
+            less: lessTask
             });
         )
         grunt.loadNpmTasks('grunt-contrib-copy');
-        grunt.registerTask('default', ['copy']);
+        grunt.loadNpmTasks('grunt-lesslint');
+        grunt.loadNpmTasks('grunt-comtrib-less');
+        grunt.registerTask('lessFiles', ['lesslint', 'less'];
+        grunt.registerTask('default', ['copy', 'lessFiles']);
     };
 })();
