b808f670221cd29b04bcf33fea21e63e99dcb0d0
Liam Armour <liamarmour1991@googlemail.com>
2015-09-10 12:24:57 +0100

    adding the lint for the JavaScript files

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,26 @@
 (function () {
     'use strict';
     var copyTask = require('./.grunt/copy-task'),
-        lessTask = require('./.grunt/less-task')
+        lessTask = require('./.grunt/less-task'),
+        jsLintTask = require('./.grunt/jshint-task');
 
     module.exports = function(grunt){
         grunt.initConfig({
             pkg: grunt.file.readJSON('package.json'),
             copy: copyTask,
             lesslint: lessTask,
-            less: lessTask
+            less: lessTask,
+            jshintlint: jsLintTask,
+            jshint: jsLintTask
             });
         )
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.loadNpmTasks('grunt-lesslint');
         grunt.loadNpmTasks('grunt-comtrib-less');
+        grunt.loadNpmTasks('jshint-stylish');
+        grunt.loadNpmTasks('grunt-comtrib-jshint');
         grunt.registerTask('lessFiles', ['lesslint', 'less'];
+        grunt.registerTask('javascriptFiles', ['jshintlint', 'jshint'];
         grunt.registerTask('default', ['copy', 'lessFiles']);
     };
 })();
