adc243c87e69a5a7c4547f33b5470dc0de99c763
Liam Armour <liamarmour1991@googlemail.com>
2015-09-10 14:17:26 +0100

    added clean task and fixed js concat

---
+++ .grunt/clean-task.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+    module.exports={
+        javascript:{
+            src:'.build/main-app/app/scripts/app.js'
+        },
+        css:{
+            src:'.build/main-app/app/css/main.css'
+        },
+        html:{
+            src:'.build/main-app/app/index.html'
+        }
+    };
+
+})();
--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,31 @@
-(function () {
+(function(){
     'use strict';
-    var copyTask = require('./.grunt/copy-task'),
-        lessTask = require('./.grunt/less-task'),
-        jsLintTask = require('./.grunt/jshint-task');
-
+    var copyTask = require('./.grunt/copy-task');
+    var lessTask = require('./.grunt/less-task');
+    var javaTask = require('./.grunt/java-task');
+    var cleanTask= require('./.grunt/clean-task');
     module.exports = function(grunt) {
         grunt.initConfig({
-            pkg: grunt.file.readJSON('package.json'),
+            pkg:grunt.file.readJSON('package.json'),
             copy: copyTask,
-            lesslint: lessTask,
-            less: lessTask,
-            jshint: jsLintTask,
-            concat: jsLintTask
-        });
+            lesslint:lessTask,
+            less:lessTask,
+            jshint:javaTask,
+            concat:javaTask,
+            clean:cleanTask,
+    
 
+        });
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.loadNpmTasks('grunt-lesslint');
-        grunt.loadNpmTasks('grunt-comtrib-less');
+        grunt.loadNpmTasks('grunt-contrib-less');
         grunt.loadNpmTasks('grunt-contrib-jshint');
         grunt.loadNpmTasks('grunt-contrib-concat');
-
-        grunt.registerTask('lessFiles', ['lesslint', 'less']);
-        grunt.registerTask('javascriptFiles', ['jshint','concat:concat']);
-        grunt.registerTask('default', ['copy', 'lessFiles']);
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.registerTask('editHtml',['clean:html','copy:html','watch']);
+        grunt.registerTask('lessFiles',['lesslint','clean:css','less','watch']);
+        grunt.registerTask('jsFiles',['jshint','clean:javascript','concat:concat','watch']);
+        grunt.registerTask('default',['watch']);
     };
 })();
