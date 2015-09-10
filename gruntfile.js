977a21ad97651906d2b6b66b9316888a38b7a77e
Liam Armour <liamarmour1991@googlemail.com>
2015-09-10 14:04:26 +0100

    fixed the errors on the grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@
         lessTask = require('./.grunt/less-task'),
         jsLintTask = require('./.grunt/jshint-task');
 
-    module.exports = function(grunt){
+    module.exports = function(grunt) {
         grunt.initConfig({
             pkg: grunt.file.readJSON('package.json'),
             copy: copyTask,
@@ -12,16 +12,16 @@
             less: lessTask,
             jshint: jsLintTask,
             concat: jsLintTask
-            });
-        )
+        });
+
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.loadNpmTasks('grunt-lesslint');
         grunt.loadNpmTasks('grunt-comtrib-less');
         grunt.loadNpmTasks('grunt-contrib-jshint');
         grunt.loadNpmTasks('grunt-contrib-concat');
 
-        grunt.registerTask('lessFiles', ['lesslint', 'less'];
-        grunt.registerTask('javascriptFiles', ['jshint','concat:concat'];
+        grunt.registerTask('lessFiles', ['lesslint', 'less']);
+        grunt.registerTask('javascriptFiles', ['jshint','concat:concat']);
         grunt.registerTask('default', ['copy', 'lessFiles']);
     };
 })();
