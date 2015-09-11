8ee62ac95ce761971fbfa082b36e293a9f262c9d
Liam Armour <liamarmour1991@googlemail.com>
2015-09-11 16:50:32 +0100

    Working on the game board so when a user clicks it will change the square to a minion and let the other person take a turn

--- .grunt/java-task.js
+++ .grunt/java-task.js
@@ -3,7 +3,7 @@
     module.exports={
         concat:{
             cwd:'',
-            src:['main-app/app/scripts/directives/**/*.js', 'main-app/app/scripts/services/**/*.js','main-app/app/scripts/controllers/**/*.js'],
+            src:['main-app/app/scripts/*.js','main-app/app/scripts/directives/**/*.js', 'main-app/app/scripts/services/**/*.js','main-app/app/scripts/controllers/**/*.js'],
             dest:'.build/main-app/app/scripts/app.js',
             expand:false
         },
--- main-app/app/scripts/controllers/game.js
+++ main-app/app/scripts/controllers/game.js
@@ -1,12 +1,29 @@
 (function () {
     'use strict';
-    angular.module('Tombola.Games.NoughtsAndCrosses', [])
+    angular.module('Tombola.Games.NoughtsAndCrosses')
 
         .controller('GameboardTextController', function($scope, $interval) {
+            var currentPlayer;
+            currentPlayer = '1';
+
             $scope.gameTitle = "Noughts and Crosses";
             $scope.playerOne = "";
             $scope.playerTwo = "";
             $scope.gameBoard='000000000';
-        });
-
+            $scope.takeTurn = function (index){
+                if ($scope.gameBoard.charAt(index)!=="0"){
+                    return;
+                }
+                if (currentPlayer === '1'){
+                    $scope.gameBoard= setCharAt($scope.gameBoard ,index,'1');
+                    document.getElementsByTagName('img')[index].setAttribute("src", "images/Crosses.gif");
+                    currentPlayer = '2';
+                }
+                else {
+                    $scope.gameBoard = setCharAt($scope.gameBoard, index, '2');
+                    document.getElementsByTagName('img')[index].setAttribute("src", "images/noughts.png");
+                    currentPlayer = '1';
+                }
+            };
+        })
 })();
