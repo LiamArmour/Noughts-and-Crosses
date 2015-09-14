8959d0113cf61bf17f1843f386280d93c6bed92f
Liam Armour <liamarmour1991@googlemail.com>
2015-09-14 09:56:26 +0100

    added the show and hide functionality

--- main-app/app/scripts/controllers/game.js
+++ main-app/app/scripts/controllers/game.js
@@ -7,8 +7,8 @@
             currentPlayer = '1';
 
             $scope.gameTitle = "Noughts and Crosses";
-            $scope.playerOne = "";
-            $scope.playerTwo = "";
+            $scope.playerOne = "Liam";
+            $scope.playerTwo = "Armour";
             $scope.gameBoard='000000000';
             $scope.takeTurn = function (index){
                 if ($scope.gameBoard.charAt(index)!=="0"){
@@ -16,14 +16,18 @@
                 }
                 if (currentPlayer === '1'){
                     $scope.gameBoard= setCharAt($scope.gameBoard ,index,'1');
-                    document.getElementsByTagName('img')[index].setAttribute("src", "images/Crosses.gif");
+                    document.getElementsByTagName('img')[index].setAttribute("src", "images/goodMinion.png");
                     currentPlayer = '2';
                 }
                 else {
                     $scope.gameBoard = setCharAt($scope.gameBoard, index, '2');
-                    document.getElementsByTagName('img')[index].setAttribute("src", "images/noughts.png");
+                    document.getElementsByTagName('img')[index].setAttribute("src", "images/badMinion.png");
                     currentPlayer = '1';
                 }
             };
+            function setCharAt(gameboardString,index,chr) {
+                if(index > gameboardString.length-1) return gameboardString;
+                return gameboardString.substr(0,index) + chr + gameboardString.substr(index+1);
+            }
         })
 })();
