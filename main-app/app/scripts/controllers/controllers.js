93f5b2fc7bdcff49633c5f47a104652cf136d00e
Liam Armour <liamarmour1991@googlemail.com>
2015-09-16 17:05:10 +0100

    Updated the player selection page

--- main-app/app/scripts/controllers/controllers.js
+++ main-app/app/scripts/controllers/controllers.js
@@ -7,11 +7,34 @@
             currentPlayer = '1';
 
             $scope.gameTitle = "Noughts and Crosses";
-            $scope.playerOne = "PlayerOne";
-            $scope.playerTwo = "PlayerTwo";
+            $scope.playerOne = "human";
+            $scope.playerTwo = "human";
             $scope.gameBoard='000000000';
             $scope.mustShow = '';
             $scope.mustShow=false;
+
+            $scope.selectPlayer = function(playerChoice){
+                if (playerChoice === 1) {
+                    $scope.playerOne = selectCharacter($scope.playerOne);
+                }
+                else {
+                    $scope.playerTwo = selectCharacter($scope.playerTwo);
+                }
+            };
+            var selectCharacter = function (playerChoice) {
+                if (playerChoice === "human") {
+                    return "pre-trained";
+                }
+                else
+                    if (playerChoice === "pre-trained") {
+                        return "random";
+                    }
+                    else {
+                        return "human";
+                    }
+            };
+
+
             $scope.makeNewGame = function(){
                 gameProxy.newGame($scope.playerOne, $scope.playerTwo).then(
                     function(data){
