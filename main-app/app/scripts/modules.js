ada2f03b96007fb89eb7bb0e4d9af70846feb0a4
Liam Armour <liamarmour1991@googlemail.com>
2015-09-17 14:31:49 +0100

    working on the .config

--- main-app/app/scripts/modules.js
+++ main-app/app/scripts/modules.js
@@ -1,5 +1,17 @@
 (function () {
     'use strict';
     angular.module('Tombola.Games.NoughtsAndCrosses', []);
-    angular.module('Tombola.Games.NoughtsAndCrosses.Proxy',[]);
+    angular.module('Tombola.Games.NoughtsAndCrosses')
+        .config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+            $UrlRouterProvider.otherwise("/playerSelection");
+            $StateProvider
+                .state('playerSelection', {
+                    url: "/playerSelection",
+                    templateUrl: "partials/playerselection.html"
+                })
+                .state('gameBaord', {
+                    url: "/gameBoard",
+                    templateUrl: "partials/gameboard.html"
+                })
+        }])
 })();
