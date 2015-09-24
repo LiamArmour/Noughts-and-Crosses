7b362a51125c1182ae8deb1850165792459cfd1a
Liam Armour <liamarmour1991@googlemail.com>
2015-09-24 11:27:13 +0100

    fixed the error with the game not working with express, but i now have errors with the constant controlling the API, this is my next fix

--- main-app/app/scripts/core/services/proxy-constants.js
+++ main-app/app/scripts/core/services/proxy-constants.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('Tombola.Games.NoughtsAndCrosses.Proxy')
+    angular.module('Tombola.Games.NoughtsAndCrosses')
         .constant('proxyConstants', {
             newGameURL : "http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame",
             takeTurnURL : "http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove"
