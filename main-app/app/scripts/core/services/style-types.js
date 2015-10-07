d2a1f2ef667a43f3f72fe96ae2b98e3aea9cc095
Liam Armour <liamarmour1991@googlemail.com>
2015-10-07 09:41:27 +0100

    finished the types

--- main-app/app/scripts/core/services/style-types.js
+++ main-app/app/scripts/core/services/style-types.js
@@ -1,5 +1,5 @@
 (function () {
     'use strict';
-    angular.module('Tombola.Games.NoughtsAndCrosses.Lobby')
+    angular.module('Tombola.Games.NoughtsAndCrosses.Core')
         .value('StyleTypes', ['minions', 'football']);
 })();
