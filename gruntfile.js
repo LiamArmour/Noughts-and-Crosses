d41f6377a2304981dbc938fa32a75247d21ada18
Liam Armour <liamarmour1991@googlemail.com>
2015-10-12 10:07:14 +0100

    Added karma to grunt file, about to split up to own file

--- gruntfile.js
+++ gruntfile.js
@@ -3,7 +3,8 @@
     var copyTask = require('./.grunt/copy-task');
     var lessTask = require('./.grunt/less-task');
     var javaTask = require('./.grunt/java-task');
-    var cleanTask= require('./.grunt/clean-task');
+    var cleanTask = require('./.grunt/clean-task');
+    var karmaTask = require('./.grunt/karma-task');
     module.exports = function(grunt) {
         grunt.initConfig({
             pkg:grunt.file.readJSON('package.json'),
@@ -35,6 +36,11 @@
                     spawn:false
                 }
 
+            },
+            karma: {
+                unit: {
+                    configFile: 'karma.conf.js'
+                }
             }
 
         });
@@ -46,6 +52,9 @@
         grunt.loadNpmTasks('grunt-contrib-clean');
         grunt.loadNpmTasks('grunt-contrib-watch');
         grunt.loadNpmTasks('grunt-express-server');
+        grunt.loadNpmTasks('grunt-karma');
+
+
         grunt.registerTask('editHtml',['clean:html','copy:html','copy:partials']);
         grunt.registerTask('lessFiles',['lesslint','clean:css','less']);
         grunt.registerTask('jsFiles',['jshint','clean:javascript','concat:concat']);
