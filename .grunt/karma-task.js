a36c93fc3f42383093f5353952a11747851e552a
Liam Armour <liamarmour1991@googlemail.com>
2015-10-13 13:39:46 +0100

    Working on testing the styles, adding the paths to karma file

--- .grunt/karma-task.js
+++ .grunt/karma-task.js
@@ -8,7 +8,10 @@
                 files: ['bower_components/angular/angular.js',
                     'bower_components/angular-mocks/angular-mocks.js',
                     'main-app/app/scripts/modules.js',
+                    'main-app/app/scripts/core/services/*.js',
                     'main-app/app/scripts/lobby/services/*.js',
+
+                    'main-app/tests/core/services/*.js',
                     'main-app/tests/lobby/services/*.js']
             }
     };
