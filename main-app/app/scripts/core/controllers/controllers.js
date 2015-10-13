35565452b1c57485914688c184bec1f9222efc65
Liam Armour <liamarmour1991@googlemail.com>
2015-10-13 13:25:06 +0100

    fixed

--- main-app/app/scripts/core/controllers/controllers.js
+++ main-app/app/scripts/core/controllers/controllers.js
@@ -11,18 +11,5 @@
                 gameModel.makeNewGame();
                 $state.go('gameBaord');
             };
-<<<<<<< HEAD
-            $scope.changePath = function() {
-                if ($scope.nextStyleName === 'Football'){
-                    $scope.stylePath='css/football.css';
-                    $scope.nextStyleName = 'Minions';
-                } else {
-                    $scope.stylePath='css/minion.css';
-                    $scope.nextStyleName = 'Football';
-                }
-
-            };
-=======
->>>>>>> master
         }]);
 })();
