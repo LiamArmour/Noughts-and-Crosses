e4c5b2ee4ece367b6b4ab52ed78c5e17c62fd472
Liam Armour <liamarmour1991@googlemail.com>
2015-10-13 13:48:08 +0100

    the toggle is not testing correctly

--- main-app/tests/lobby/services/player-selection.test.js
+++ main-app/tests/lobby/services/player-selection.test.js
@@ -14,6 +14,16 @@
             constants.player2Type.should.equal('human');
         });
 
+        it('Ensures the rotation of player 1 functions correctly', function () {
+            constants.selectPlayer(1);
+            constants.player1Type.should.equal('human');
+            constants.selectPlayer(1);
+            constants.player1Type.should.equal('pre-trained');
+            constants.selectPlayer(1);
+            constants.player1Type.should.equal('random');
+        });
+
+
 
     });
 })();
