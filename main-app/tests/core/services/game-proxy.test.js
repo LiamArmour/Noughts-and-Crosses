c47decb2010b3708ac43b508a18cac1efc625f4a
Liam Armour <liamarmour1991@googlemail.com>
2015-10-14 14:40:37 +0100

    both tests complete

--- main-app/tests/core/services/game-proxy.test.js
+++ main-app/tests/core/services/game-proxy.test.js
@@ -15,7 +15,7 @@
             });
         });
 
-        it('Ensures the newGame function is working and returns values', function(){
+        it('Ensures the newgame function is working and returns values', function(){
             var theResponse = {'outcome':'Continue','gameboard':'000000000','winner':0};
             httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame" , {'player1':"human", 'player2':"human"})
                 .respond(theResponse);
@@ -28,11 +28,12 @@
             result.should.be.deep.equal(theResponse);
         });
 
-        it('Ensures the newGame function is working and returns values', function(){
-            var theResponse = {'outcome':'Continue','gameboard':'000000000','winner':0};
-            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame" , {'player1':"human", 'player2':"human"})
+        it('Ensures the makemove function is working and returns values', function(){
+            var theResponse = {'outcome':'Continue','gameboard':'100000000','winner':0};
+            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove" , {"playerNumber": "1",
+                "chosenSquare": "0"})
                 .respond(theResponse);
-            var returnedPromise = proxy.newGame("human", "human");
+            var returnedPromise = proxy.playerTurn("1", "0");
             var result;
             returnedPromise.then(function(response){
                 result = response;
