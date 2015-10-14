7610c1861e568d6bf31c13b3f1d4af6025b0369f
Liam Armour <liamarmour1991@googlemail.com>
2015-10-14 14:22:43 +0100

    working on the before each

--- main-app/tests/core/services/game-proxy.test.js
+++ main-app/tests/core/services/game-proxy.test.js
@@ -2,12 +2,18 @@
     'use strict';
 
     describe("Testing-the-game-proxy", function() {
-        var gameServiceProxy;
+        var gameProxy,
+            httpServer,
+            requestHnadler;
         beforeEach(function() {
             module('Tombola.Games.NoughtsAndCrosses.Core');
             inject(function($injector) {
-                gameServiceProxy = $injector.get('GameProxy');
-
+                gameProxy = $injector.get('GameProxy');
+                httpServer = $injector.get('$httpBackend');
+                requestHnadler = $httpBackend.when('POST','http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove')
+                    .respond(function(){
+                        return [200, {board: '000000000'}]
+                    });
             });
         });
 
