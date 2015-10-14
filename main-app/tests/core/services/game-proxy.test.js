3b1e83c5251ea903553314f5b8205d38e2d12856
Liam Armour <liamarmour1991@googlemail.com>
2015-10-14 14:38:17 +0100

    using the code and going to change it for the take turn function

--- main-app/tests/core/services/game-proxy.test.js
+++ main-app/tests/core/services/game-proxy.test.js
@@ -28,6 +28,19 @@
             result.should.be.deep.equal(theResponse);
         });
 
+        it('Ensures the newGame function is working and returns values', function(){
+            var theResponse = {'outcome':'Continue','gameboard':'000000000','winner':0};
+            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame" , {'player1':"human", 'player2':"human"})
+                .respond(theResponse);
+            var returnedPromise = proxy.newGame("human", "human");
+            var result;
+            returnedPromise.then(function(response){
+                result = response;
+            });
+            httpBackend.flush();
+            result.should.be.deep.equal(theResponse);
+        });
+
         afterEach(function() {
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
