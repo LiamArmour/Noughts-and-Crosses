878b0a6dc58f7ca072c6b729c299e9471514cdb3
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 14:46:22 +0100

    Attempted to add the gameModel to the mocks file

--- main-app/tests/mocks/mocks.js
+++ main-app/tests/mocks/mocks.js
@@ -2,5 +2,18 @@
 var mocks = {
     ProxyConstants:{
         baseURL:"http://eutaveg-01.tombola.emea:35000/api/v1.0/"
-    }
+    },
+
+    GameModel:{
+        currentGameState: '',
+        playerWinner: '',
+        makeNewGame: function () {},
+        gameboard: {
+            player1: 'human',
+            player2: 'human',
+            board: '000000000',
+            playerTurn: 1
+        },
+    },
+
 };
