ec302adf6148ec6ccb7cd3a1510869401b152a1f
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 11:08:46 +0100

    injecting a mock into my test

--- main-app/tests/core/services/game-proxy.test.js
+++ main-app/tests/core/services/game-proxy.test.js
@@ -2,10 +2,9 @@
     'use strict';
     describe('Testing my game proxy', function () {
         var httpBackend,
-            proxyName;
-        beforeEach(function(){
+            proxyName,
+            sandbox;
 
-        });
         beforeEach(function(){
             module('Tombola.Games.NoughtsAndCrosses.Core');
             inject(function( $injector){
@@ -13,7 +12,12 @@
                 proxyName = $injector.get('GameProxy');
 
             });
+
+            sandbox = sinon.sandbox.create();
+            baseURL = sinon.sandbox.mock(mocks.ProxyConstants);
+
         });
+
         it('Ensures the newgame function is working and returns values', function(){
             var theResponse = {'outcome':'Continue','gameboard':'000000000','winner':0};
             httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame" , {'player1':"human", 'player2':"human"})
