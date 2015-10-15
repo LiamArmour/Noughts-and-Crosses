7bc1d1ec6a5db893a0fc33e99c1379b4eb483682
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 12:07:45 +0100

    The file is not picking up the controller name :(

--- main-app/tests/core/controllers/controller.test.js
+++ main-app/tests/core/controllers/controller.test.js
@@ -1,3 +1,17 @@
-/**
- * Created by liam.armour on 15/10/15.
- */
+(function () {
+    'use strict';
+    describe('Testing the main controller', function () {
+        var constants;
+        beforeEach(function () {
+            module('Tombola.Games.NoughtsAndCrosses.Core');
+            inject(function ($injector) {
+                constants = $injector.get('MainController');
+            });
+        });
+
+        it('Ensures the game title is correct', function () {
+            constants.gameTitle.should.equal('Noughts and Crosses');
+        });
+    });
+
+})();
