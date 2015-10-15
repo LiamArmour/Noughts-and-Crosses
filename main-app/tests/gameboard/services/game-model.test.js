cdb11137de465cec56bb95c9f40609201878fc0f
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 15:47:13 +0100

    Different error this time

--- main-app/tests/gameboard/services/game-model.test.js
+++ main-app/tests/gameboard/services/game-model.test.js
@@ -14,11 +14,11 @@
             module(function ($provide) {
                 $provide.value('GameModel');
                 $provide.value('PlayerSelection', mocks.PlayerSelection);
-                $provide.value('PlayerSelection', mocks.PlayerSelection);
+                $provide.value('GameProxy', mocks.GameProxy);
             });
             sandbox = sinon.sandbox.create();
             gameModel = sinon.sandbox.mock(mocks.PlayerSelection);
-
+            gameProxy= sinon.sandbox.mock(mocks.Gameproxy);
 
             inject(function ($injector, $state, $interval) {
                 playerSelection = $injector.get('PlayerSelection');
@@ -30,18 +30,63 @@
         });
 
         it('Ensures the current player is player 1', function () {
-            mocks.CoreData.currentPlayer.should.equal('1');
+            mocks.currentPlayer.should.equal('1');
         });
-
-        it('Ensures the current player is player 1', function () {
-            gameProxy.gameBoard='000000000';
-        });
-
-        it('Ensures the current player is player 1', function () {
-            gameProxy.gameWinner = '';
-        });
-
 
     });
 
 }());
+
+//every under needs testing above
+//(function () {
+//    'use strict';
+//    angular.module('Tombola.Games.NoughtsAndCrosses.Game')
+//        .service('GameModel', ['$state', '$interval', 'PlayerSelection','GameProxy', function($state, $interval, playerSelection, gameProxy) {
+//            var me = this,
+//                outcome = 'Continue',
+//                checkGameEnded = function(){
+//                    $interval(function(){
+//                        if (outcome === "Win") {
+//                            $state.go('gameWin');
+//                        } else if (outcome === "Draw") {
+//                            $state.go('gameDraw');
+//                        }
+//                    },5000, 1);
+//                },
+//                updateGameBoard = function(data){
+//                    me.gameBoard = data.gameboard;
+//                    outcome = data.outcome;
+//                    me.gameWinner = data.winner;
+//                    checkGameEnded();
+//                };
+//            me.playerSelection = playerSelection;
+//            me.currentPlayer = '1';
+//            me.gameBoard='000000000';
+//            me.gameWinner = '';
+//            me.makeNewGame = function(){
+//                gameProxy.apiCall("newgame",{"player1" : playerSelection.player1Type, "player2" : playerSelection.player2Type})
+//                    .then(function(data){
+//                        me.currentPlayer = playerSelection.getStartingPlayer();
+//                        updateGameBoard(data);
+//                    },
+//                    function(data){
+//                        console.log(data);
+//                    });
+//            };
+//            me.takeTurn = function (index){
+//                if (me.gameBoard[index] !== '0') {
+//                    return;
+//                }
+//                gameProxy.apiCall("makemove", {"playerNumber": me.currentPlayer, "chosenSquare": index})
+//                    .then(function(data){
+//                        updateGameBoard(data);
+//                        if (playerSelection.isHumanVsHuman()){
+//                            me.currentPlayer = me.currentPlayer === 1 ? 2: 1;
+//                        }
+//                    },
+//                    function(data){
+//                        console.log('make move failed: ' + data );
+//                    });
+//            };
+//        }]);
+//})();
