3d50a12f7501f64adb2bb52920e5a9fd1590846f
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 15:09:35 +0100

    using some old code as a template

--- main-app/tests/gameboard/services/game-model.test.js
+++ main-app/tests/gameboard/services/game-model.test.js
@@ -1,3 +1,39 @@
-/**
- * Created by liam.armour on 15/10/15.
- */
+(function () {
+    'use strict';
+    describe('Testing my game proxy', function () {
+        var httpBackend,
+            gameProxy;
+
+        beforeEach(function () {
+            module('Tombola.Games.NoughtsAndCrosses.Core');
+            module(function ($provide) {
+                $provide.value('ProxyConstants', mocks.ProxyConstants);
+            });
+
+            inject(function ($injector) {
+                httpBackend = $injector.get('$httpBackend');
+                gameProxy = $injector.get('GameProxy');
+            });
+
+        });
+
+        it('Ensures the newgame function is working and returns values', function () {
+            var theResponse = {'outcome': 'Continue', 'gameboard': '000000000', 'winner': 0};
+            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame", {'player1': "human", 'player2': "human"})
+                .respond(theResponse);
+            var returnedPromise = gameProxy.apiCall("newgame",{'player1' : "human", 'player2' : "human"});
+            var result;
+            returnedPromise.then(function (response) {
+                result = response;
+                result.should.be.deep.equal(theResponse);
+            });
+            httpBackend.flush();
+        });
+
+
+
+
+
+    });
+
+}());
