25ea1dc4c21aa733a9dd6609599bb7657e06142c
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 15:26:04 +0100

    Updating the mocks to take in gameModel stuff

--- main-app/tests/mocks/mocks.js
+++ main-app/tests/mocks/mocks.js
@@ -2,5 +2,11 @@
 var mocks = {
     ProxyConstants:{
         baseURL:"http://eutaveg-01.tombola.emea:35000/api/v1.0/"
+    },
+    GameModel:{
+        playerSelection: '1',
+        currentPlayer: '1',
+        gameBoard: '000000000',
+        gameWinner: ''
     }
 };
