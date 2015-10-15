20d915706690293a5fd52a7043bdc03ba1a18796
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 15:30:59 +0100

    Player selection mocks done

--- main-app/tests/mocks/mocks.js
+++ main-app/tests/mocks/mocks.js
@@ -1,14 +1,24 @@
 'use strict';
 var mocks = {
-    ProxyConstants:{
-        baseURL:"http://eutaveg-01.tombola.emea:35000/api/v1.0/"
+    ProxyConstants: {
+        baseURL: "http://eutaveg-01.tombola.emea:35000/api/v1.0/"
     },
-    GameModel:{
+    GameModel: {
         playerSelection: '1',
         currentPlayer: '1',
         gameBoard: '000000000',
         gameWinner: '',
-        takeTurn: function(){},
-        makeNewGame: function(){}
+        takeTurn: function () {
+        },
+        makeNewGame: function () {
+        }
+    },
+
+    PlayerSelection: {
+        player1Type: 0,
+        player2Type: 0,
+        selectPlayer: function () {},
+        getStartingPlayer: function () {},
+        isHumanVsHuman: function () {}
     }
 };
