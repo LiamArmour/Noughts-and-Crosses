90b2cac55df302f8ea23744cbf21beb39219346c
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 11:40:35 +0100

    Getting closer with the test

--- main-app/tests/core/services/game-proxy.test.js
+++ main-app/tests/core/services/game-proxy.test.js
@@ -2,54 +2,56 @@
     'use strict';
     describe('Testing my game proxy', function () {
         var httpBackend,
-            proxyName,
-            sandbox;
+            gameProxy;
 
-        beforeEach(function(){
+        beforeEach(function () {
             module('Tombola.Games.NoughtsAndCrosses.Core');
-            inject(function( $injector){
-                httpBackend = $injector.get('$httpBackend');
-                proxyName = $injector.get('GameProxy');
-
-                module(function($provide){
-                $provide.service('ProxyConstants', mocks.ProxyConstants);
+            module(function ($provide) {
+                $provide.value('ProxyConstants', mocks.ProxyConstants);
             });
 
-            sandbox = sinon.sandbox.create();
-            ProxyConstants = sinon.sandbox.mock(mocks.ProxyConstants);
+            inject(function ($injector) {
+                httpBackend = $injector.get('$httpBackend');
+                gameProxy = $injector.get('GameProxy');
+            });
 
         });
 
-        it('Ensures the newgame function is working and returns values', function(){
-            var theResponse = {'outcome':'Continue','gameboard':'000000000','winner':0};
-            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame" , {'player1':"human", 'player2':"human"})
-                .respond(theResponse);
-            var returnedPromise = proxyName.newGame("human", "human");
-            var result;
-            returnedPromise.then(function(response){
-                result = response;
+            it('Ensures the newgame function is working and returns values', function () {
+                var theResponse = {'outcome': 'Continue', 'gameboard': '000000000', 'winner': 0};
+                httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame",
+                    {'player1': "human", 'player2': "human"});
+                    .respond(theResponse);
+                var returnedPromise = gameProxy.apiCall("newgame",{'player1' : human, 'player2' : human});
+                var result;
+                returnedPromise.then(function (response) {
+                    result = response;
+                    result.should.be.deep.equal(theResponse);
+                });
+                httpBackend.flush();
+            });
+
+            it('Ensures the makemove function is working and returns values', function () {
+                var theResponse = {'outcome': 'Continue', 'gameboard': '100000000', 'winner': 0};
+                httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove", {
+                    "playerNumber": "1",
+                    "chosenSquare": "0"
+                })
+                    .respond(theResponse);
+                var returnedPromise = gameProxy.playerTurn("1", "0"),
+                    result;
+                returnedPromise.then(function (response) {
+                    result = response;
+                });
+                httpBackend.flush();
                 result.should.be.deep.equal(theResponse);
             });
-            httpBackend.flush();
-        });
 
-        it('Ensures the makemove function is working and returns values', function(){
-            var theResponse = {'outcome':'Continue','gameboard':'100000000','winner':0};
-            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove" , {"playerNumber": "1",
-                "chosenSquare": "0"})
-                .respond(theResponse);
-            var returnedPromise = proxyName.playerTurn("1", "0"),
-                result;
-            returnedPromise.then(function(response){
-                result = response;
+            afterEach(function () {
+                httpBackend.verifyNoOutstandingExpectation();
+                httpBackend.verifyNoOutstandingRequest();
             });
-            httpBackend.flush();
-            result.should.be.deep.equal(theResponse);
+
         });
 
-        afterEach( function(){
-            httpBackend.verifyNoOutstandingExpectation();
-            httpBackend.verifyNoOutstandingRequest();
-        });
-    });
 }());
