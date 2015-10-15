82dbdf00f7c3deb47eb9b6c7a48b242b288fb55d
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 12:17:30 +0100

    working on controller testing

--- main-app/tests/core/controllers/controller.test.js
+++ main-app/tests/core/controllers/controller.test.js
@@ -1,16 +1,20 @@
 (function () {
     'use strict';
     describe('Testing the main controller', function () {
-        var constants;
-        beforeEach(function () {
-            module('Tombola.Games.NoughtsAndCrosses.Core');
-            inject(function ($injector) {
-                constants = $injector.get('MainController');
+        var controller,
+            scope;
+        beforeEach(function () {module('Tombola.Games.NoughtsAndCrosses.Core');
+            inject(function ($controller, $rootScope) {
+                scope = $rootScope.$new();
+                controller = $controller('MainController', {
+                    $scope: scope
+                });
+
             });
         });
 
         it('Ensures the game title is correct', function () {
-            constants.gameTitle.should.equal('Noughts and Crosses');
+            controller.gameTitle.should.equal('Noughts and Crosses');
         });
     });
 
