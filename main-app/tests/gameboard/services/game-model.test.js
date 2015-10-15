9ea8a23c8b0f2d7a5d314e7f9408c4f7dc5e2b2e
Liam Armour <liamarmour1991@googlemail.com>
2015-10-15 15:15:11 +0100

    got a little bit more working

--- main-app/tests/gameboard/services/game-model.test.js
+++ main-app/tests/gameboard/services/game-model.test.js
@@ -2,32 +2,28 @@
     'use strict';
     describe('Testing the game model service', function () {
         var playerSelection,
-            gameProxy;
+            gameProxy,
+            state,
+            interval;
 
+        beforeEach(module('ui.router'));
         beforeEach(function () {
             module('Tombola.Games.NoughtsAndCrosses.Game');
             module(function ($provide) {
                 $provide.value('GameModel');
             });
 
-            inject(function ($injector) {
+            inject(function ($injector, $state, $interval) {
                 playerSelection = $injector.get('PlayerSelection');
                 gameProxy = $injector.get('GameProxy');
+                state = $state;
+                interval = $interval;
             });
 
         });
 
-        it('Ensures the newgame function is working and returns values', function () {
-            var theResponse = {'outcome': 'Continue', 'gameboard': '000000000', 'winner': 0};
-            httpBackend.expectPOST("http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame", {'player1': "human", 'player2': "human"})
-                .respond(theResponse);
-            var returnedPromise = gameProxy.apiCall("newgame",{'player1' : "human", 'player2' : "human"});
-            var result;
-            returnedPromise.then(function (response) {
-                result = response;
-                result.should.be.deep.equal(theResponse);
-            });
-            httpBackend.flush();
+        it('Ensures the current player is player 1', function () {
+            gameProxy.currentPlayer = '1';
         });
 
 
