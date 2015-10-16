1fa684ecfa9b2fde5164292aaa3d7a3414c19fe9
Liam Armour <liamarmour1991@googlemail.com>
2015-10-16 09:57:58 +0100

    still trying to get my tests to work

--- main-app/tests/gameboard/services/game-model.test.js
+++ main-app/tests/gameboard/services/game-model.test.js
@@ -33,17 +33,25 @@
             mocks.currentPlayer.should.equal('1');
         });
 
+        it('Ensures the outcome starts at Continue', function () {
+            gameProxy.outcome.should.equal('Continue');
+        });
+
+        it('Ensures the gameboard loads up empty', function () {
+            mocks.gameBoard.should.equal('000000000');
+        });
+
     });
 
 }());
 
 //every under needs testing above
+
 //(function () {
 //    'use strict';
 //    angular.module('Tombola.Games.NoughtsAndCrosses.Game')
 //        .service('GameModel', ['$state', '$interval', 'PlayerSelection','GameProxy', function($state, $interval, playerSelection, gameProxy) {
 //            var me = this,
-//                outcome = 'Continue',
 //                checkGameEnded = function(){
 //                    $interval(function(){
 //                        if (outcome === "Win") {
@@ -53,16 +61,17 @@
 //                        }
 //                    },5000, 1);
 //                },
+
 //                updateGameBoard = function(data){
 //                    me.gameBoard = data.gameboard;
 //                    outcome = data.outcome;
 //                    me.gameWinner = data.winner;
 //                    checkGameEnded();
 //                };
+
 //            me.playerSelection = playerSelection;
-//            me.currentPlayer = '1';
-//            me.gameBoard='000000000';
 //            me.gameWinner = '';
+
 //            me.makeNewGame = function(){
 //                gameProxy.apiCall("newgame",{"player1" : playerSelection.player1Type, "player2" : playerSelection.player2Type})
 //                    .then(function(data){
@@ -73,6 +82,7 @@
 //                        console.log(data);
 //                    });
 //            };
+
 //            me.takeTurn = function (index){
 //                if (me.gameBoard[index] !== '0') {
 //                    return;
