b20d39972c78403d74a804fadc946a88b8a612e5
Liam Armour <liamarmour1991@googlemail.com>
2015-10-16 10:21:35 +0100

    Added the mocks into a function

--- main-app/tests/mocks/mocks.js
+++ main-app/tests/mocks/mocks.js
@@ -1,28 +1,28 @@
-'use strict';
-var mocks = {
-    ProxyConstants: {
-        baseURL: "http://eutaveg-01.tombola.emea:35000/api/v1.0/"
-    },
-    GameModel: {
-        playerSelection: '1',
-        currentPlayer: '1',
-        gameBoard: '000000000',
-        gameWinner: '',
-        takeTurn: function () {
+(function () {
+    'use strict';
+    var mocks = {
+        ProxyConstants: {
+            baseURL: "http://eutaveg-01.tombola.emea:35000/api/v1.0/"
         },
-        makeNewGame: function () {
+        GameModel: {
+            playerSelection: '1',
+            currentPlayer: '1',
+            gameBoard: '000000000',
+            gameWinner: '',
+            takeTurn: function () {
+            },
+            makeNewGame: function () {
+            }
+        },
+        PlayerSelection: {
+            player1Type: 0,
+            player2Type: 0,
+            selectPlayer: function () {},
+            getStartingPlayer: function () {},
+            isHumanVsHuman: function () {}
+        },
+        GameProxy: {
+            apiCall: function () {}
         }
-    },
-
-    PlayerSelection: {
-        player1Type: 0,
-        player2Type: 0,
-        selectPlayer: function () {},
-        getStartingPlayer: function () {},
-        isHumanVsHuman: function () {}
-    },
-
-    GameProxy: {
-        apiCall: function () {}
-    }
-};
+    };
+})();
