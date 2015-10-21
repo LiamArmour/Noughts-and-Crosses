ea29a27b4a75da4fca52d029d4246c7226c6ff54
Liam Armour <liamarmour1991@googlemail.com>
2015-10-21 15:13:49 +0100

    not sure if i should keep this second test? keeping it in a commit incase i want it later

--- main-app/tests/core/directives/gameboard-square.test.js
+++ main-app/tests/core/directives/gameboard-square.test.js
@@ -13,7 +13,6 @@
         });
 
         it('Ensure that the game board square directive inputs data for each of the 9 squares', function() {
-
             var directiveElement = '<game-board-square square-number="0" class="cell"></game-board-square>';
 
             $rootScope.gameBoard = function(){return [1,0,0,0,0,0,0,0,0]};
@@ -24,7 +23,6 @@
 
             subElement.attr('class').should.equal('cell player');
             subElement.attr('ng-click').should.equal('model.takeTurn(0)');
-
         });
 
         it('Ensure that the game board square directive inputs data for square 1', function() {
