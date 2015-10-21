52301a06abbadc6bf719772ba54ab03b4cf93837
Liam Armour <liamarmour1991@googlemail.com>
2015-10-21 15:10:34 +0100

    Added a separate test to test if it will populate another square

--- main-app/tests/core/directives/gameboard-square.test.js
+++ main-app/tests/core/directives/gameboard-square.test.js
@@ -12,7 +12,7 @@
             });
         });
 
-        it('Ensure that the game board square directive inputs date for each of the 9 squares', function() {
+        it('Ensure that the game board square directive inputs data for each of the 9 squares', function() {
 
             var directiveElement = '<game-board-square square-number="0" class="cell"></game-board-square>';
 
@@ -24,6 +24,21 @@
 
             subElement.attr('class').should.equal('cell player');
             subElement.attr('ng-click').should.equal('model.takeTurn(0)');
+
+        });
+
+        it('Ensure that the game board square directive inputs data for square 1', function() {
+
+            var directiveElement = '<game-board-square square-number="1" class="cell"></game-board-square>';
+
+            $rootScope.gameBoard = function(){return [0,1,0,0,0,0,0,0,0]};
+            var element = $compile(directiveElement)($rootScope);
+            $rootScope.$digest();
+
+            var subElement = element.find('div');
+
+            subElement.attr('class').should.equal('cell player');
+            subElement.attr('ng-click').should.equal('model.takeTurn(1)');
 
         });
         
