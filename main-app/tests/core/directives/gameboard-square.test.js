89f2bd1ea44b07671418bc9b06fcfe54d43ad3b1
Liam Armour <liamarmour1991@googlemail.com>
2015-10-21 14:19:19 +0100

    got the before each working

--- main-app/tests/core/directives/gameboard-square.test.js
+++ main-app/tests/core/directives/gameboard-square.test.js
@@ -1,4 +1,17 @@
 (function () {
     'use strict';
+    describe('Gamebaord sqaure directive test', function() {
+        var $compile,
+            $rootScope;
+
+        beforeEach(function(){
+            module('');
+            inject(function(_$compile_, $rootScope){
+                $compile = _$compile_;
+                $rootScope = _$rootScope_;
+            })
+        });
+        
+    });
 
 }());
