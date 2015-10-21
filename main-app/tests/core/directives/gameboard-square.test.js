cb6db9f3ba48f236c04fab4ee4e880193e3c8357
Liam Armour <liamarmour1991@googlemail.com>
2015-10-21 14:37:52 +0100

    Broken directive test, attempting to fix

--- main-app/tests/core/directives/gameboard-square.test.js
+++ main-app/tests/core/directives/gameboard-square.test.js
@@ -5,14 +5,22 @@
             $rootScope;
 
         beforeEach(function(){
-            module('');
-            inject(function(_$compile_, $rootScope){
+            module('Tombola.Games.NoughtsAndCrosses.Game');
+            inject(function(_$compile_, _$rootScope_){
                 $compile = _$compile_;
                 $rootScope = _$rootScope_;
-            })
+            });
         });
 
-        it('Ensure that the gameboard square directive inputs each of the 9 squares', function() {
+        it.only('Ensure that the gameboard square directive inputs each of the 9 squares', function() {
+            
+            var directiveElement = '<div class="cell player{{model.gameBoard['+ attributes.squareNumber+']}}" ng-click="model.takeTurn('+attributes.squareNumber+')"></div>';
+
+            $rootScopeProvider.gameModel = {gameBoardSquare: '0'};
+            var element = $compile(directiveElement)(rootScope);
+            $rootScope.$digest();
+
+            element[0].toString().should.equal('[object HTMLImageElement]');
 
         });
         
