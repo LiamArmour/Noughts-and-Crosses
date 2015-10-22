3e8745bafb87c9ca4a9cdf73966100d38b330938
Liam Armour <liamarmour1991@googlemail.com>
2015-10-22 10:12:46 +0100

    Working on fixing the controller test

--- main-app/tests/core/controllers/controller.test.js
+++ main-app/tests/core/controllers/controller.test.js
@@ -5,6 +5,8 @@
             controller,
             scope,
             gameModel,
+            sandbox,
+            stateSpy,
             styleSelection;
 
         beforeEach(module('ui.router'));
@@ -14,16 +16,20 @@
 
             //Mock these in GameModel,StyleSelection
 
-            inject(function ($controller, $rootScope, $state, GameModel, StyleSelection) {
+            inject(function ($injector, $controller, $rootScope, $state, GameModel, StyleSelection) {
                 scope = $rootScope.$new();
                 state = $state;
                 controller = $controller('MainController', {
-                    $scope: scope
+                    $scope: scope,
+                    $state:mocks.$state
                 });
                 gameModel = $injector('GameModel');
                 styleSelection = $injector('StyleSelection');
 
             });
+
+            sandbox =sinon.sandbox.create();
+            stateSpy = sinon.sandbox.spy(mocks.$state,'go');
 
         });
 
@@ -33,3 +39,20 @@
     });
 
 })();
+
+
+//(function () {
+//    'use strict';
+//    angular.module('Tombola.Games.NoughtsAndCrosses.Core')
+//
+//        .controller('MainController', ['$scope','$state','GameModel','StyleSelection',  function($scope, $state, gameModel, styleSelection) {
+//            $scope.gameTitle = 'Noughts and Crosses';
+//            $scope.displayStyle = styleSelection;
+//            $scope.changeStyle = styleSelection.toggleCss;
+//
+//            $scope.makeNewGame= function (){
+//                gameModel.makeNewGame();
+//                $state.go('gameBaord');
+//            };
+//        }]);
+//})();
