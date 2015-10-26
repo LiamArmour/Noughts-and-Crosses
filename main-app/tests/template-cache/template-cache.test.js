d9d6df8fa9535c185c4037c6d32c2486c7211359
Liam Armour <liamarmour1991@googlemail.com>
2015-10-26 10:38:06 +0000

    Created all the files for one test

--- main-app/tests/template-cache/template-cache.test.js
+++ main-app/tests/template-cache/template-cache.test.js
@@ -1,3 +1,41 @@
-/**
- * Created by liam.armour on 26/10/15.
- */
+(function () {
+    'use strict';
+    describe('Testing all of states within the application', function () {
+        var sandbox,
+            $timeout,
+            $state,
+            $rootscope,
+            $templateCache,
+            spyonState;
+
+        beforeEach(function () {
+            module('Tombola.Games.NoughtsAndCrosses');
+            sandbox = sinon.sandbox.create();
+            inject(function ($injector, _$state_, _$timeout_, _$rootScope_, _$templateCache_) {
+                $timeout = _$timeout_;
+                $state = _$state_;
+                $rootscope = _$rootScope_;
+                $templateCache = _$templateCache_;
+            });
+            $templateCache.put('partials/player-selection.html', 'partials/player-selection.html');
+            $templateCache.put('partials/game-board.html', 'partials/game-board.html');
+            $templateCache.put('partials/game-rules.html', 'partials/game-rules.html');
+            $templateCache.put('partials/game-about.html', 'partials/game-about.html');
+            $templateCache.put('partials/game-draw.html', 'partials/game-draw.html');
+
+            spyonState = sinon.sandbox.spy(mocks.fakeState, 'go');
+            $rootscope.$digest();
+
+        });
+
+        it('Make sure the default state is playerselection', function () {
+            var state = $state.get('playerSelection');
+            should.exist(state);
+            state.url.should.equal('/playerSelection');
+            state.controller.should.equal('LobbyController');
+            state.templateProvider($templateCache).should.equal($templateCache.get('partials/player-selection.html'));
+        });
+
+    });
+
+})();
