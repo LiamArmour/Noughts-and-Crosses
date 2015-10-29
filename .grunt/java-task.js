95fbd8928cef86fe20abfe667d6459e0ceb08a24
Liam Armour <liamarmour1991@googlemail.com>
2015-10-29 12:29:52 +0000

    Updating master

--- .grunt/java-task.js
+++ .grunt/java-task.js
@@ -3,7 +3,7 @@
     module.exports={
         concat:{
             cwd:'',
-            src:['main-app/app/scripts/modules.js','main-app/app/scripts/core/**/*.js','main-app/app/scripts/lobby/**/*.js','main-app/app/scripts/gameboard/**/*.js'],
+            src:['main-app/app/scripts/modules.js','main-app/app/scripts/template-cache.js','main-app/app/scripts/core/**/*.js','main-app/app/scripts/lobby/**/*.js','main-app/app/scripts/gameboard/**/*.js'],
             dest:'.build/scripts/app.js',
             expand:false
         },
