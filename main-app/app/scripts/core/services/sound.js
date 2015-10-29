975ab5b0768e34e2a1f1206a06f77bdecda54325
Liam Armour <liamarmour1991@googlemail.com>
2015-10-29 11:53:28 +0000

    Sound service

---
+++ main-app/app/scripts/core/services/sound.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+    angular.module('Tombola.Games.NoughtsAndCrosses.Core')
+        .service('Sound', ['$timeout', function($timeout){
+            var me = this,
+                audio = '',
+                stopAudio = function(){
+                    audio.get(0).pause();
+                    me.currentlyPlaying = false;
+                };
+
+            me.currentlyPlaying = false;
+
+            me.playSound = function(startTime, duration){
+                if(!me.currentlyPlaying) {
+                    audio = angular.element('#soundplayer');
+                    audio.prop('currentTime', startTime);
+                    audio.get(0).play();
+                    me.currentlyPlaying = true;
+                    $timeout(stopAudio, duration, true);
+                }
+            };
+        }]);
+})();
